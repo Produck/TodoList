8a6a2710022a0bf396ba318d215d877e3d3d6a0b
suyoung <suyoung154@gmail.com>
2018-08-11 18:38:02 +0900

    completed layout

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from '../TodoItem';
+
+const TodoList = () => {
+    return (
+        <div>
+            <TodoItem done>리액트 공부하기</TodoItem>
+            <TodoItem>컴포넌트 스타일링 해보기</TodoItem>
+        </div>
+    );
+};
+
+export default TodoList;
