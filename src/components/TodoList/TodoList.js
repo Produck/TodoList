6a3901b9a80513e9dddb7caa26dfb672d6ada45d
suyoung <suyoung154@gmail.com>
2018-08-11 19:51:34 +0900

    optimized performance

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TodoItem from '../TodoItem';
 
-const TodoList = ({todos, onToggle, onRemove}) => {
-    const todoList = todos.map(
-        todo => (
-            <TodoItem key={todo.id} done={todo.done} onToggle={() => onToggle(todo.id)} onRemove={() => onRemove(todo.id)}>{todo.text}</TodoItem>
-        )
-    )
+class TodoList extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.todos !== nextProps.todos;
+    }
 
-    return (
-        <div>
-            {todoList}
-        </div>
-    );
-};
+    render() {
+        const {todos, onToggle, onRemove} = this.props;
 
+        const todoList = todos.map(todo => (
+            <TodoItem
+                key={todo.id}
+                done={todo.done}
+                onToggle={() => onToggle(todo.id)}
+                onRemove={() => onRemove(todo.id)}>{todo.text}</TodoItem>
+        ))
+
+        return (
+            <div>
+                {todoList}
+            </div>
+        );
+    };
+}
 export default TodoList;
